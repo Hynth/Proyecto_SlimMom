0ec6431fcc698c6be6f15b26ba9375733ad44233
davidfsora <davidsora0204@gmail.com>
2024-01-19 00:38:22 -0500

    First push David

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';  // Asegúrate de que la ruta sea correcta
-import App from './calorieCalculator/app.js';  // Asegúrate de que la ruta sea correcta
+import { BrowserRouter } from "react-router-dom";
+import App from './components/App.jsx';  // Asegúrate de que la ruta sea correcta
 
 const root = createRoot(document.getElementById('root'));
 
 // Renderiza tu aplicación usando root.render
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
