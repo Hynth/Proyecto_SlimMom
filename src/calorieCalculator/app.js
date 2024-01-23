2595b036e62cab2b46207f8c3fca2ea644d56975
Fuasma2727 <jmamgb127@gmail.com>
2024-01-23 17:44:12 -0500

    diario-calculadora

---
+++ src/calorieCalculator/app.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import CaloriesCalculator from './caloriesCalculator.js';
+import '../css/stylesCalories.css'; // Importa el archivo de estilos CSS
+
+const App = () => {
+  return (
+    <div>
+      <CaloriesCalculator />
+      {/* Otros componentes o contenido */}
+    </div>
+  );
+};
+
+export default App;
