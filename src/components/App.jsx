a13fb27c204e27b64c477194530745ba38b56a47
Fuasma2727 <jmamgb127@gmail.com>
2024-01-24 18:42:08 -0500

    24Ene24

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
-import Home from '../pages/Home/Home.jsx'
+import Home from '../pages/Home/Home'
 import RegistrationPage from '../pages/RegistrationPage';
 import LoginPage from '../pages/LoginPage';
 import DiaryPage from '../pages/DiaryPage';
-import Header from './Header/Header';
-import styles from './Header/Header.module.css'
-import CaloriesCalculator from './calorieCalculator/caloriesCalculator';
+import CalculatorPage from '../pages/CalculatorPage'
 
 const App = () => {
   return (
     <div>
-			<CaloriesCalculator/>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />} />
+				<Route
+					path="/register"
+					element={<RegistrationPage />} />
+				<Route
+					path="/login"
+					element={<LoginPage />} />
+				<Route
+					path="/diary"
+					element={<DiaryPage />} />
+				<Route
+					path="/calculator"
+					element={<CalculatorPage />} />
+			</Routes>
     </div>
   );
 };
 
-export default App;
+
