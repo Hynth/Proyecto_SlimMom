e9fb108150b06ef9efd9f3456317a99e3faffa32
Jonathan Alexis Jiménez Triviño <jonnyj1216@gmail.com>
2024-01-25 01:20:15 -0500

    opacity

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import CalculatorForm from '../../components/CalculatorForm/CalculatorForm'
+import CalculatorForm from '../../components/CalculatorForm/CalculatorForm';
 import sH from '../Home/Home.module.css';
 
 const Home = () => {
-	return (
-		<div className={sH.mainPage}>
-			<Header currentPage={'Main'} />
-			<CalculatorForm currentPage={'Main'} />
-		</div>
-	);
-}
+  return (
+    <div className={sH.mainPage}>
+      <Header currentPage={'Main'} />
+      <CalculatorForm currentPage={'Main'} />
+    </div>
+  );
+};
 
 export default Home;
